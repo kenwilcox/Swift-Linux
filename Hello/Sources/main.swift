52df016a7d4e8cac9c40a412aa883057c8e29daf
Kenneth Wilcox <wilcoxk@gmail.com>
2016-08-30 22:47:53 -0700

    Process changed to CommandLine

--- Hello/Sources/main.swift
+++ Hello/Sources/main.swift
@@ -1,6 +1,6 @@
-if Process.arguments.count != 2 {
-    print("Usage: \(Process.arguments[0]) NAME")
+if CommandLine.arguments.count != 2 {
+    print("Usage: \(CommandLine.arguments[0]) NAME")
 } else {
-    let name = Process.arguments[1]
+    let name = CommandLine.arguments[1]
     sayHello(name: name)
 }
