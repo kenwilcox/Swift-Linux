fe83ca839802c8eecd731f7e982c89af597b5f7b
Kenneth Wilcox <wilcoxk@gmail.com>
2016-10-13 17:41:33 -0700

    changed it up a bit

--- Hello/Sources/main.swift
+++ Hello/Sources/main.swift
@@ -1,6 +1,9 @@
-if CommandLine.arguments.count != 2 {
+//print("arg count: \(CommandLine.arguments.count)")
+if CommandLine.arguments.count == 1 {
     print("Usage: \(CommandLine.arguments[0]) NAME")
 } else {
-    let name = CommandLine.arguments[1]
+    //let name = CommandLine.arguments[1]
+    let args = CommandLine.arguments[1..<CommandLine.arguments.count];
+    let name = args.joined(separator: " ")
     sayHello(name: name)
 }
